a6253e2260671bbf418d431b055b60670bdcb87f
Mathieu PATUREL <australie.p@gmail.com>
2021-09-05 10:31:51 +1000

    implement pager

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,18 @@
 import "./css/index.css"
 import "./css/facts.css"
 import "./css/tools.css"
+import "./css/pager.css"
 
 import "./ts/database.ts"
 import { Facts } from "./ts/components/facts"
 import { Tools } from "./ts/components/tools"
 import { EM } from "./ts/EventManager"
+import { Pager } from "./ts/components/pager"
 
 document.addEventListener("DOMContentLoaded", () => {
     const tools = new Tools()
     const facts = new Facts()
+    const pager = new Pager()
     
     EM.emit("set-case", 0)
 })
