253d11aa017d4bb58aaaa0297b36cc5a2feb8c82
Mathieu PATUREL <australie.p@gmail.com>
2021-09-07 22:02:58 +1000

    support images as facts

--- src/ts/components/facts.ts
+++ src/ts/components/facts.ts
@@ -1,3 +1,4 @@
+import "../../css/facts.css";
 import { CaseData, database } from "../database";
 import { EM } from "../EventManager";
 import { assert, querySelector } from "../utils";
@@ -5,11 +6,13 @@ import { assert, querySelector } from "../utils";
 export class Facts {
   elements: {
     factsList: HTMLUListElement;
+    imagesList: HTMLUListElement;
   };
 
   constructor() {
     this.elements = {
       factsList: querySelector("#facts-list"),
+      imagesList: querySelector("#images-list"),
     };
 
     EM.on("set-case", (index: number) => {
@@ -23,6 +26,17 @@ export class Facts {
 
   _displayData(data: CaseData) {
     this.elements.factsList.innerHTML = "";
+    this.elements.imagesList.innerHTML = "";
+
+    if (data.images)
+      for (let imageLink of data.images) {
+        const item = document.createElement("li");
+        const image = document.createElement("img");
+        image.src = imageLink;
+        item.appendChild(image);
+        this.elements.imagesList.appendChild(item);
+      }
+
     for (let itemText of data.facts) {
       const item = document.createElement("li");
       item.textContent = itemText;
